73aee7c11644c09b2588d6940b141226acded25b
Precious-Bob <preciousbobmanuel501@gmail.com>
2025-05-29 05:34:18 +0100

    feat(cart): implement cart controller with CRUD operations and JWT protection

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
+import { AddItemDto } from 'src/dto/addItem.dto';
+import { UpdateItemDto } from 'src/dto/updateItem.dto';
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+import { User } from 'src/decorators/user.decorator';
 
 @Controller('cart')
+@UseGuards(JwtAuthGuard)
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Post('items')
+  async addItem(
+    @User('userId') userId: string,
+    @Body() addItemDto: AddItemDto,
+  ) {
+    return this.cartService.addItem(userId, addItemDto);
+  }
+
+  @Put('items/:itemId')
+  async updateItem(
+    @User('userId') userId: string,
+    @Param('itemId') itemId: string,
+    @Body() updateItemDto: UpdateItemDto,
+  ) {
+    return this.cartService.updateItem(userId, itemId, updateItemDto);
+  }
+
+  @Delete('items/:itemId')
+  async removeItem(
+    @User('userId') userId: string,
+    @Param('itemId') itemId: string,
+  ) {
+    return this.cartService.removeItem(userId, itemId);
+  }
+
+  @Get()
+  async getCart(@User('userId') userId: string) {
+    return this.cartService.getCartDetails(userId);
+  }
 }
